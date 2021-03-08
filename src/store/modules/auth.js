0fcaf3f8247acce53f1719b82bfb567e452f0e5e
bakir1 <37544754+bakir1@users.noreply.github.com>
2021-03-08 16:38:18 +0100

    set up and add new auth

---
+++ src/store/modules/auth.js
@@ -0,0 +1,15 @@
+const state = {
+    token: null
+};
+
+const getters = {
+    isLoggedIn: state => !!state.token
+};
+
+const actions = {};
+
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
